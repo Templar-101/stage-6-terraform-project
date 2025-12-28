a7bade1b6b54543882172c08683a4e9fb60e6350
Templar-101 <hcokorie@gmail.com>
2025-12-28 13:52:06 +0000

    Initial Stage 6 Terraform project

---
+++ main.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+resource "random_id" "bucket_id" {
+  byte_length = 4
+}
+
+resource "aws_s3_bucket" "stage6_bucket" {
+  bucket = "henry-stage6-${random_id.bucket_id.hex}"
+
+  tags = {
+    Name = "Stage6Bucket"
+    Env  = "Dev"
+  }
+}
+
+provider "aws" {
+  profile = "dev"
+  region  = "eu-west-2"
+}
